706e530eb9d22ea2a87a51cd5a0a6bd67260ae6e
sisiam08 <mohammadsisiam08@gmail.com>
2026-06-15 07:45:53 +0600

    add env variables

---
+++ src/env.ts
@@ -0,0 +1,14 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import * as z from "zod";
+
+export const env = createEnv({
+  server: {
+    BACKEND_URL: z.string().url(),
+    API_URL: z.string().url(),
+  },
+  client: {},
+  runtimeEnv: {
+    BACKEND_URL: process.env.BACKEND_URL,
+    API_URL: process.env.API_URL,
+  },
+});
